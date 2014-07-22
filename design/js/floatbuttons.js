62d85a1a3cb234acbcf92c79b8195a68e19f9658
anck69 <kynsilehto.aleksi@gmail.com>
2014-07-22 23:59:20 +0300

    Showing floating buttons always

--- design/js/floatbuttons.js
+++ design/js/floatbuttons.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 	floatBody = $('body');
 	floatElement = $('#top-buttons');
 	$(window).trigger('scroll');
+	$(window).trigger('resize');
 });
 
 function floatOnScroll() {
@@ -16,37 +17,17 @@ function floatOnScroll() {
 			1000
 		);
 	}
-	
-	if($(window).scrollTop() >= floatElement.outerHeight()) {
-		if(floatElement.hasClass('button-group-float')) {
-			return;
-		}
-		
-		floatElement.toggle();
-		floatElement.addClass('button-group-float');
-		floatBody.css('margin-top', floatElement.outerHeight());
-		floatBody.css('margin-bottom', floatElement.outerHeight());
-		floatElement.fadeToggle();
-	}
-	else {
-		if(!floatElement.hasClass('button-group-float')) {
-			return;
-		}
-		
-		floatElement.toggle();
-		floatElement.removeClass('button-group-float');
-		floatBody.removeAttr('style');
-		floatElement.fadeToggle();
-	}
 }
 
 $(window).scroll(function() {
-	if(	floatElement.css('opacity') > 0.2
-		&& $(window).scrollTop() >= floatElement.outerHeight()
-		&& $(window).scrollTop() + $(window).height() < $(document).height() - 50) {
+	if(floatElement.css('opacity') > 0.2 && $(window).scrollTop() + $(window).height() < $(document).height() - 50) {
 		floatElement.css('opacity', 0.2);
 	}
 	
 	clearTimeout(floatTimeout);
 	floatTimeout = setTimeout(floatOnScroll, 200);
+});
+
+$(window).resize(function() {
+	floatBody.css('margin-bottom', floatElement.outerHeight());
 });
