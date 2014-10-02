c0c216d46d5a0418df3ea3a85b2e38377abe090d
anck69 <kynsilehto.aleksi@gmail.com>
2014-10-02 19:42:13 +0300

    Removed laggy opacity animation on scroll

--- design/js/floatbuttons.js
+++ design/js/floatbuttons.js
@@ -1,51 +1,11 @@
 // Using float prefix
-
-var floatActive = true,
-	floatBody,
-	floatElement,
-	floatScrollTimeout,
-	floatResizeTimeout,
-	floatOpacityDelay;
-
-$(document).ready(function() {
-	floatBody = $('body');
-	floatElement = $('.float-buttons.list-view');
-	
-	if(floatElement.length == 0) {
-		floatActive = false;
-		return;
-	}
-	
-	$(window).trigger('scroll');
-	$(window).trigger('resize');
-});
-
-function floatOnScroll() {
-	floatElement.stop().animate({'opacity': 1.0}, 1000);
-}
-
-$(window).scroll(function() {
-	if(!floatActive) {
-		return;
-	}
-	
-	if(floatElement.css('opacity') > 0.2 && $(window).scrollTop() + $(window).height() < $(document).height() - 50) {
-		floatElement.css('opacity', 0.2);
-	}
-	
-	clearTimeout(floatScrollTimeout);
-	floatScrollTimeout = setTimeout(floatOnScroll, 1000);
-});
+var floatResizeTimeout;
 
 function floatOnResize() {
-	floatBody.css('margin-bottom', floatElement.outerHeight());
+	$('body').css('margin-bottom', $('.float-buttons.list-view').outerHeight());
 }
 
 $(window).resize(function() {
-	if(!floatActive) {
-		return;
-	}
-	
 	clearTimeout(floatResizeTimeout);
 	floatResizeTimeout = setTimeout(floatOnResize, 50);
 });
