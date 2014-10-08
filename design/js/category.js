129bdc35dc44685df4c2eec8ac3315a0053886b0
anck69 <kynsilehto.aleksi@gmail.com>
2014-10-08 23:32:26 +0300

    Several UI improvements

--- design/js/category.js
+++ design/js/category.js
@@ -3,6 +3,12 @@
 $(document).ready(function() {
 	$('.categorySelect').fastClick(function(e) {
 		e.preventDefault();
-		$('.categoryContainer').slideToggle();
+		$('.categoryContainer').toggleClass('visible');
+	});
+	
+	$('.categoryContainer .button').fastClick(function() {
+	    $(this).parent().find('.button').each(function() {
+	        $(this).removeClass('active');
+	    });
 	});
 });
