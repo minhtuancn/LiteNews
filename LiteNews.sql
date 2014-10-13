9999d645694b3be4c0ae3e5035e3c082eafdc999
anck69 <kynsilehto.aleksi@gmail.com>
2014-10-13 21:45:40 +0300

    Update LiteNews.sql (category)

--- LiteNews.sql
+++ LiteNews.sql
@@ -1,95 +1,41 @@
--- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: litenews
--- ------------------------------------------------------
--- Server version	5.5.37-0ubuntu0.12.04.1
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `Article`
---
 
-DROP TABLE IF EXISTS `Article`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Article` (
+CREATE TABLE IF NOT EXISTS `Article` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `WebsiteID` tinyint(4) NOT NULL,
   `URL` text NOT NULL,
+  `Category` tinyint(3) unsigned NOT NULL DEFAULT '1',
   `ListTitle` tinytext NOT NULL,
   `ArticleTitle` tinytext NOT NULL,
   `SubTitle` text,
   `Timestamp` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `ArticleParagraph`
---
-
-DROP TABLE IF EXISTS `ArticleParagraph`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ArticleParagraph` (
+CREATE TABLE IF NOT EXISTS `ArticleParagraph` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ArticleID` int(11) NOT NULL,
   `Paragraph` text NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `ArticleID` (`ArticleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `Config`
---
-
-DROP TABLE IF EXISTS `Config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Config` (
+CREATE TABLE IF NOT EXISTS `Config` (
   `ID` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
   `ParentID` smallint(4) unsigned NOT NULL,
   `Name` tinytext NOT NULL,
   `Value` tinytext NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `FPC`
---
-
-DROP TABLE IF EXISTS `FPC`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `FPC` (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Parameters` text NOT NULL,
-  `Content` longtext NOT NULL,
-  `Timestamp` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Feedback`
---
-
-DROP TABLE IF EXISTS `Feedback`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Feedback` (
+CREATE TABLE IF NOT EXISTS `Feedback` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Type` tinyint(4) NOT NULL,
   `Email` text NOT NULL,
@@ -98,62 +44,38 @@ CREATE TABLE `Feedback` (
   `IP` tinytext NOT NULL,
   `Timestamp` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `GeoCity`
---
+CREATE TABLE IF NOT EXISTS `FPC` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Parameters` text NOT NULL,
+  `Content` longtext NOT NULL,
+  `Timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `GeoCity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `GeoCity` (
+CREATE TABLE IF NOT EXISTS `GeoCity` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `City` tinytext NOT NULL,
   `Lat` float NOT NULL,
   `Lon` float NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `Log`
---
-
-DROP TABLE IF EXISTS `Log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Log` (
+CREATE TABLE IF NOT EXISTS `Log` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `IP` tinytext NOT NULL,
   `Timestamp` int(11) NOT NULL,
   `URL` text NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `UpdateTime`
---
-
-DROP TABLE IF EXISTS `UpdateTime`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `UpdateTime` (
+CREATE TABLE IF NOT EXISTS `UpdateTime` (
   `WebsiteID` tinyint(4) NOT NULL,
   `Timestamp` int(11) NOT NULL,
   PRIMARY KEY (`WebsiteID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-08-06 17:07:26
