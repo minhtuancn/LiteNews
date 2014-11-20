97f44f68175c6e21b3c88fcffdc661f15c2c1ad9
anck69 <kynsilehto.aleksi@gmail.com>
2014-11-20 18:48:29 +0200

    Fixed webkit scroll issue on fixed element

--- design/js/floatbuttons.js
+++ design/js/floatbuttons.js
@@ -2,10 +2,23 @@
 var floatResizeTimeout;
 
 function floatOnResize() {
-	$('body').css('margin-bottom', $('.float-buttons.list-view').outerHeight());
+	$('body').css('margin-bottom', $('body>.floatButtons').outerHeight());
 }
 
 $(window).resize(function() {
 	clearTimeout(floatResizeTimeout);
 	floatResizeTimeout = setTimeout(floatOnResize, 50);
+});
+
+/* fucking webkit */
+$(document).ready(function () {
+        $('.ajaxArticle').scroll(function () {
+            var contentBox = $(this).find('.contentBox');
+            if(contentBox.css('opacity') != 1.0) {
+                contentBox.css('opacity', 1.0);
+            }
+            else {
+                contentBox.css('opacity', 0.99);
+            }
+        });
 });
